be43bfdd9458144497a174b0eb1be35a2d6f1ec5
Jo1m8a3 <jazimermann@gmail.com>
2015-05-15 09:41:43 -0300

    v0.0.0

--- service/modal-service.js
+++ service/modal-service.js
@@ -3,20 +3,28 @@ angular.module('modal-service', ['$rootScope', '$modal', '$modalStack'])
 
     var modalInstance;
 
-    this.open = function (templateUrl, size) {
+    this.confirm = function (message, subMessage, yesText, noText, templateUrl, size) {
+      
       var modalScope = $rootScope.$new(true);
+      modalScope.message = message;
+      modalScope.subMessage = subMessage;
+      modalScope.yesText = yesText;
+      modalScope.noText;
+      
+      return this.open(modalScope, templateUrl, size); 
+    }
 
+    this.open = function (modalScope, templateUrl, size) {
+
+      this.closeAll();
+      
       var modalOptions = $modal.open({
         templateUrl: templateUrl,
         size: size,
         scope: modalScope
       });
 
-      modalInstance = $modal.open(modalOptions).result;
-    };
-
-    this.close = function () {
-      modalInstance.close();
+      return $modal.open(modalOptions).result;
     };
 
     this.closeAll = function () {
