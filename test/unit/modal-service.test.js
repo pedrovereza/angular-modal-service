4a2cdb5cbabf7125b19ebb97ee97a866b3134bc9
Jo1m8a3 <jazimermann@gmail.com>
2015-05-18 10:06:13 -0300

    v0.0.0

---
+++ test/unit/modal-service.test.js
@@ -0,0 +1,12 @@
+describe("modal-service", function () {
+
+  beforeEach(function () {
+    console.log("executou");
+  });
+
+  it("", function () {
+    console.log("executou");
+    expect(true).toBe(true);
+  });
+
+});
